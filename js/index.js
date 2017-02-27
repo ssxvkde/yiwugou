fde29d6059d8469b5757803d3ca56d3bb5130d18
hyj <hyj@example.com>
2017-02-27 20:45:38 +0800

    01-头部

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$(function(){
+	
+	//头部语言栏
+	$('.language').mouseover(function(){
+		$(this).css('border','0').css('overflow','visible');
+		$('.language ul').css('border','1px solid gray');
+	});
+	
+})
